8f56d8662bc683d9978c54caf667def3c2ecf086
wenyan <sabertobihwy@gmail.com>
2024-08-30 17:54:22 -0400

    js function

--- js/index.js
+++ js/index.js
@@ -60,7 +60,7 @@ class UIGoods{
     }
 
     overThreshold(){
-        return getTotalPrice() >= this.deliveryThreshold
+        return this.getTotalPrice() >= this.deliveryThreshold
     }
 
     hasGoodsInCart(){
@@ -78,10 +78,21 @@ class UI{
     constructor(){
         this.UIGoods = new UIGoods()
         this.dom = {
-            goodsListHTML :  document.querySelector('.goods-list')
+            goodsListHTML :  document.querySelector('.goods-list'),
+            footerNumHTML: document.querySelector('.footer-car-badge'),
+            footerTotalHTML: document.querySelector('.footer-car-total'),
+            footerTipHTML: document.querySelector('.footer-car-tip'),
+            footerPayHTML: document.querySelector('.footer-pay'),
+            cartHTML:document.querySelector('.footer-car')
         }
-       // console.log(document.querySelector('.goods-list'));
+       var rect = this.dom.cartHTML.getBoundingClientRect()
+       this.targetCart = {
+            x: rect.left+ rect.width /2 ,
+            y: rect.top + rect.height /5 
+       }
         this.createHTML()
+        this.updateTip()
+        this.addEventListener()
     }
 
     createHTML(){
@@ -107,10 +118,10 @@ class UI{
                     ${g.data.price}
                 </p>
                 <p class="goods-btns">
-                   <svg class="icon" aria-hidden="true">
+                   <svg class="icon hidden inactive" aria-hidden="true">
                     <use xlink:href="#icon-jianhao"></use>
                   </svg>
-                    <span>${g.number}</span>
+                    <span class="hidden inactive number">${g.number}</span>
                     <svg class="icon" aria-hidden="true">
                     <use xlink:href="#icon-jiahao"></use>
                   </svg>
@@ -122,6 +133,87 @@ class UI{
         }
         this.dom.goodsListHTML.innerHTML = html
     }
+
+    overThreshold(){
+        return this.UIGoods.overThreshold()
+    }
+
+    getNumber(index){
+        return this.UIGoods.goodsList[index].number
+    }
+
+    getTotalPrice(){
+        return this.UIGoods.getTotalPrice()
+    }
+
+    getTotalNumber(){
+        return this.UIGoods.getTotalNumber()
+    }
+
+    increase(index){
+        this.UIGoods.increase(index);
+        this.updateGoodsItem(index);
+        this.updateFooter();
+        this.addAnimate();
+    }
+
+    decrease(index){
+        this.UIGoods.decrease(index);
+        this.updateGoodsItem(index);
+        this.updateFooter();
+        this.addAnimate();
+    }
+
+    updateTip(){
+        this.dom.footerTipHTML.innerHTML = `配送费¥${this.UIGoods.deliveryFee}`
+    }
+
+    updateGoodsItem(index){
+        var itemHTML = this.dom.goodsListHTML.children[index]
+        console.log(itemHTML)
+        var hiddenHTMLs = itemHTML.querySelectorAll('.hidden')
+        if(this.UIGoods.isChosen(index)){
+            hiddenHTMLs.forEach(e=>{
+                e.classList.remove('inactive')
+            })
+            var number = itemHTML.querySelector('.number')
+            number.innerHTML = this.getNumber(index)
+        }else{
+           hiddenHTMLs.forEach(e=>{
+                e.classList.add('inactive');
+            })
+        }
+    }
+
+    updateFooter(){
+        var cartNumHTML = this.dom.footerNumHTML
+        cartNumHTML.innerHTML = this.getTotalNumber()
+        this.dom.footerTotalHTML.innerHTML = this.getTotalPrice()
+        var footerPayHTML_a = this.dom.footerPayHTML.querySelector('a')
+        var footerPayHTML_span = this.dom.footerPayHTML.querySelector('span')
+        if(this.overThreshold()){
+            footerPayHTML_a.classList.remove('inactive')
+            footerPayHTML_span.classList.add('inactive')
+        }else
+            footerPayHTML_a.classList.add('inactive')
+            footerPayHTML_span.innerHTML = `还差¥${this.UIGoods.deliveryThreshold - this.getTotalPrice()}元起送`
+    }
+
+    addEventListener(){
+        // listen animation
+        this.dom.cartHTML.addEventListener('animationend',()=>{
+            this.dom.cartHTML.classList.remove('animate')
+        })
+    }
+    
+   addAnimate(){
+            this.dom.cartHTML.classList.add('animate')
+   }
+
 }
+    
+
+
 
 var u = new UI()
+
