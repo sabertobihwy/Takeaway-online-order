c8b223032dbecd0cd0fc84656fa99999f0062bca
wenyan <sabertobihwy@gmail.com>
2024-08-30 23:27:51 -0400

    js complete basic function

--- js/index.js
+++ js/index.js
@@ -40,6 +40,8 @@ class UIGoods{
     }
 
     decrease(index){
+        console.log(index)
+        console.log(this.goodsList[index])
         this.goodsList[index].decrease()
     }
 
@@ -118,13 +120,17 @@ class UI{
                     ${g.data.price}
                 </p>
                 <p class="goods-btns">
-                   <svg class="icon hidden inactive" aria-hidden="true">
-                    <use xlink:href="#icon-jianhao"></use>
-                  </svg>
+                    <span data-index="${i}" class="jianhao" >
+                        <svg class="icon hidden inactive" aria-hidden="true">
+                            <use xlink:href="#icon-jianhao"></use>
+                        </svg>
+                    </span>
                     <span class="hidden inactive number">${g.number}</span>
-                    <svg class="icon" aria-hidden="true">
-                    <use xlink:href="#icon-jiahao"></use>
-                  </svg>
+                    <span data-index="${i}" class="jiahao">
+                        <svg class="icon" aria-hidden="true">
+                            <use xlink:href="#icon-jiahao"></use>
+                        </svg>
+                    </div>
                 </p>
             </div>
             </div>
@@ -155,6 +161,7 @@ class UI{
         this.updateGoodsItem(index);
         this.updateFooter();
         this.addAnimate();
+        this.jump(index);
     }
 
     decrease(index){
@@ -170,7 +177,7 @@ class UI{
 
     updateGoodsItem(index){
         var itemHTML = this.dom.goodsListHTML.children[index]
-        console.log(itemHTML)
+       // console.log(itemHTML)
         var hiddenHTMLs = itemHTML.querySelectorAll('.hidden')
         if(this.UIGoods.isChosen(index)){
             hiddenHTMLs.forEach(e=>{
@@ -194,9 +201,11 @@ class UI{
         if(this.overThreshold()){
             footerPayHTML_a.classList.remove('inactive')
             footerPayHTML_span.classList.add('inactive')
-        }else
+        }else{
             footerPayHTML_a.classList.add('inactive')
+            footerPayHTML_span.classList.remove('inactive')
             footerPayHTML_span.innerHTML = `还差¥${this.UIGoods.deliveryThreshold - this.getTotalPrice()}元起送`
+        }
     }
 
     addEventListener(){
@@ -204,16 +213,81 @@ class UI{
         this.dom.cartHTML.addEventListener('animationend',()=>{
             this.dom.cartHTML.classList.remove('animate')
         })
+
+
     }
     
    addAnimate(){
             this.dom.cartHTML.classList.add('animate')
    }
 
+   jump(index){
+    var itemHTML = this.dom.goodsListHTML.children[index].querySelector('.jiahao')
+   //var itemHTML = document.querySelector('.jiahao')
+   // console.log(itemHTML)
+    var from = itemHTML.getBoundingClientRect()
+   // console.log(from)
+    var fromAxios = {
+        x: from.left,
+        y: from.top
+    }
+   // console.log(fromAxios)
+    // create cart
+    var span = document.createElement('span')
+    span.className = 'addToCartAnimation'
+    span.style.transform = `translateX(${fromAxios.x}px)`
+    span.innerHTML = `<svg class="icon trans" aria-hidden="true" style="transform: translateY(${fromAxios.y}px)">
+                        <use xlink:href="#icon-jiahao"></use>
+                     </svg>`
+
+    // 获取 <body> 的第一个子元素
+    var firstChild = document.body.firstChild;
+
+    // 将 <div> 插入到 <body> 的最前面
+    document.body.insertBefore(span, firstChild);
+   // console.log(span)
+
+    // reflow
+    span.clientWidth
+
+    // 飞到destination
+    span.style.transform = `translateX(${this.targetCart.x}px)`
+    span.children[0].style.transform = `translateY(${this.targetCart.y}px)`
+   // console.log(span.children[0])
+
+    var that = this
+    span.addEventListener('transitionend',function(){
+        span.remove()
+        that.addAnimate()
+    },{
+        once: true // only trigger once
+    })
+    
+   }
+
 }
     
 
-
-
 var u = new UI()
+
+u.dom.goodsListHTML.addEventListener('click',(e)=>{
+    // if(e.target.classList.contains('jianhao')){
+    //     var index = +e.target.getAttribute('data-index')
+    //     u.decrease(index)
+    // }else if(e.target.classList.contains('jiahao')) {
+    //     u.increase(index)
+    // }
+
+    var index; 
+    var jia = e.target.closest('.jiahao')
+    var jian = e.target.closest('.jianhao')
+    if(jia){
+        index = +jia.getAttribute('data-index')
+        u.increase(index)
+    }else if(jian){
+        index = +jian.getAttribute('data-index')
+        u.decrease(index)
+        
+    }
+})
 
